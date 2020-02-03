42e3fcbc1fc8b027036735b7c07814649921a7c4
叶枫 <645381995@qq.com>
2020-02-03 15:00:33 +0800

    test

--- src/YForm/ItemChildren.tsx
+++ src/YForm/ItemChildren.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Form } from 'antd';
 import { forEach, omit } from 'lodash';
-import { YFormItemProps } from './Items';
 import { FormItemProps } from 'antd/lib/form';
+import { YFormItemProps } from './Items';
 
 export default (props: YFormItemProps) => {
-    const { children, addonAfter, ...rest } = props;
-    let _required: boolean | undefined = false;
-    // 根据 rules required 判断外部 Form.Item 是否必填
-    forEach(props.rules, item => {
-        if ('required' in item) _required = item.required;
-    });
+  const { children, addonAfter, ...rest } = props;
+  let _required: boolean | undefined = false;
+  // 根据 rules required 判断外部 Form.Item 是否必填
+  forEach(props.rules, item => {
+    if ('required' in item) _required = item.required;
+  });
 
-    return children ? (
-        <Form.Item required={_required} {...omit(rest, ['name', 'rules', 'dependencies', 'shouldUpdate'])}>
-            <Form.Item noStyle {...rest}>
-                {children as FormItemProps['children']}
-            </Form.Item>
-            <React.Fragment>{addonAfter}</React.Fragment>
-        </Form.Item>
-    ) : null;
+  return children ? (
+      <Form.Item
+          required={_required}
+          {...omit(rest, ['name', 'rules', 'dependencies', 'shouldUpdate'])}
+      >
+          <Form.Item noStyle {...rest}>
+              {children as FormItemProps['children']}
+          </Form.Item>
+          <>{addonAfter}</>
+      </Form.Item>
+  ) : null;
 };

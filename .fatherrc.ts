4e353119c8c32fd77c21b3a1669ca57faaeeb103
叶枫 <645381995@qq.com>
2020-02-05 15:14:51 +0800

    ui 完成

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -8,9 +8,17 @@ const options: IBundleOptions = {
 	extraBabelPlugins: [['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
 	doc: {
 		typescript: true,
+		title: 'yform-demo',
+		repository: 'https://github.com/crazyair/yform-demo',
 		files: './**/*.{md,markdown,mdx}',
-		theme: 'docz-theme-umi',
-		themeConfig: { codemirrorTheme: 'dracula' },
+		theme: 'docz-theme-umi-yform',
+		themeConfig: {
+			codemirrorTheme: 'docz-light',
+			menus: [
+				{ title: '发布日志', link: 'https://github.com/crazyair/yform-demo/releases' },
+				{ title: 'Github', link: 'https://github.com/crazyair/yform-demo' },
+			],
+		},
 	},
 } as any;
 

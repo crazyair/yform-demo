a0cd380861f4b41748fa1fa0bbbe57776f4a36c2
叶枫 <645381995@qq.com>
2020-02-05 11:02:18 +0800

    cssModules false

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,17 +1,18 @@
 import { IBundleOptions } from 'father';
 
 const options: IBundleOptions = {
-    entry: 'src/index.tsx',
-    // cjs: 'rollup',
-    esm: 'rollup',
-    preCommit: { eslint: true, prettier: true },
-    extraBabelPlugins: [['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
-    doc: {
-        typescript: true,
-        files: './**/*.{md,markdown,mdx}',
-        theme: 'docz-theme-umi',
-        themeConfig: { codemirrorTheme: 'dracula' },
-    },
+	entry: 'src/index.tsx',
+	// cjs: 'rollup',
+	esm: 'rollup',
+	preCommit: { eslint: true, prettier: true },
+	cssModules: false,
+	extraBabelPlugins: [['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+	doc: {
+		typescript: true,
+		files: './**/*.{md,markdown,mdx}',
+		theme: 'docz-theme-umi',
+		themeConfig: { codemirrorTheme: 'dracula' },
+	},
 } as any;
 
 export default options;

e4bb75cbffbd03c4c1c3f3c301005292926b47d8
叶枫 <645381995@qq.com>
2020-02-27 16:07:26 +0800

    demo

--- change.sh
+++ change.sh
@@ -1,3 +1,18 @@
-changelog=$(cat CHANGELOG.md)
+# changelog=$(cat CHANGELOG.md)
+MD=$(cat CHANGELOG.md)
+VERSION=0.4.0
+# changelog=$(cat <CHANGELOG.md | grep '\S*' )
+
+
+# BIZ_PROJECTS=$(cat <$BASE_PATH/update.sh | grep '^BIZ_PROJECTS="\S*"' | grep -o '\[\S*\]')
+
+
+# changelog=$(echo "$VERSION" | xargs -i sed -n '/^#\s\'"{}"'.*$/,/^#\s\([^[:space:]]\+\).*$/{//!p}' $MD)
+# changelog=$(echo "$VERSION")
+changelog=$(echo "$MD" | xargs )
+
 echo $changelog
+# changelog="${changelog//'%'/'%25'}"
+# changelog="${changelog//$'\n'/'%0A'}"
+# changelog="${changelog//$'\r'/'%0D'}"
 echo "::set-output name=changelog::$changelog"

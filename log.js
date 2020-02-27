df4ad09a98c4766bb200ec06b922b36129139b41
叶枫 <645381995@qq.com>
2020-02-27 16:55:18 +0800

    demo

--- log.js
+++ log.js
@@ -7,7 +7,13 @@ const stringFromFile = path => {
     return readFileSync(path, 'utf-8');
 };
 
-const dd = stringFromFile('./CHANGELOG.md');
+let dd = stringFromFile('./CHANGELOG.md');
+// # changelog="${changelog//'%'/'%25'}"
+// # changelog="${changelog//$'\n'/'%0A'}"
+// # changelog="${changelog//$'\r'/'%0D'}"
+dd = dd.replace(/%/g, '%25');
+dd = dd.replace(/\n/g, '%0A');
+dd = dd.replace(/\r/g, '%0D');
 // const demo = '::set-output name=changelog::123';
 const demo = `::set-output name=changelog::${dd}`;
 

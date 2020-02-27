db5aa84eab5f35eb551d1966af7b2f620a367c0b
叶枫 <645381995@qq.com>
2020-02-27 16:36:07 +0800

    demo

--- log.js
+++ log.js
@@ -1,4 +1,12 @@
-const demo = '::set-output name=changelog::123';
+import { readFileSync } from 'fs';
+
+const stringFromFile = path => {
+    return readFileSync(path, 'utf-8');
+};
+
+const dd = stringFromFile('./CHANGELOG.md');
+// const demo = '::set-output name=changelog::123';
+const demo = `::set-output name=changelog::${dd}`;
 
 // export default demo;
 
